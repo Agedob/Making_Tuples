d7a57fc364db70ea1b01c38691430ed8250d18dc
agedob <agedob888@gmail.com>
2018-02-06 19:16:02 -0800

    done

--- server.py
+++ server.py
@@ -3,4 +3,8 @@ my_dict = {
   "Michael": "(999) 999-9999",
   "Jay": "(777) 777-7777"
 }
-
+def tup(arr):
+    for key,value in arr.iteritems():
+        v = (key,value)
+        print v
+tup(my_dict)
